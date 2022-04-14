f6c9276615ae9acf7efdfa09086d6732d070b2f5
Ryan <ryan.chung-kam-chung@mths.ca>
2022-04-14 16:20:54 +0100

    fixed formatting

--- store02.s
+++ store02.s
@@ -20,9 +20,7 @@ main:
     ldr r2, =myvar2 @ r2 <- &myvar2
     mov r3, #4 @ r3 <- 4
     str r3, [r2] @ *r2 <- r3
-    ldr r1, =myvar1 @ r1 <- &myvar1
     ldr r1, [r1] @ r1 <- *r1
-    ldr r2, =myvar2 @ r2 <- &myvar2
     ldr r2, [r2] @ r2 <- *r2
     add r0, r1, r2
     bx lr
